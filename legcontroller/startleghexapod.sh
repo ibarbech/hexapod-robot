4d78400bc4650ce293811a7bd52cdc55b47ef3e6
ibarbech <ibarbech@alumnos.unex.es>
2016-06-28 11:59:13 +0200

    add sh starthexapod

---
+++ legcontroller/startleghexapod.sh
@@ -0,0 +1,6 @@
+bin/LegControllerComp config1 &
+bin/LegControllerComp config2 &
+bin/LegControllerComp config3 &
+bin/LegControllerComp config4 &
+bin/LegControllerComp config5 &
+bin/LegControllerComp config6 &
